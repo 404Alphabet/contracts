c2375bec4ea11d7be9f10f48348fecffcd19fa59
mehranhydary <mehranhydary@users.noreply.github.com>
2024-02-16 00:09:58 -0500

    Now you see me

--- src/SJ741/Emeralds.sol
+++ src/SJ741/Emeralds.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.0;
 
 import {SJ741} from "./SJ741.sol";
 
-contract Emeralds is SJ741 {
-    constructor() SJ741("SJ741 Emeralds", "EMERALD", "https://raw.githubusercontent.com/SerecThunderson/assets/main/emeralds/metadata/") {}
+contract Alphabet is SJ741 {
+    constructor() SJ741("SJ741 Alphabet", "ALPHABET", "TBD") {}
 }
